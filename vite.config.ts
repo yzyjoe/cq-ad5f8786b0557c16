0c80a36ab2ce76db788baa9b50b9b95d5fed453f
yzyjoe <lucaseumesmo007@gmail.com>
2026-09-06 03:56:12 -0300

    Versiona estilos para atualizar cache

--- vite.config.ts
+++ vite.config.ts
@@ -9,11 +9,6 @@ export default defineConfig({
     emptyOutDir: true,
     rollupOptions: {
       input: "site.html",
-      output: {
-        entryFileNames: "assets/site.js",
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name][extname]",
-      },
     },
   },
 });
